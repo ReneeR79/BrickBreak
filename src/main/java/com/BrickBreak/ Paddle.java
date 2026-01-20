5c8ea63db06188212d7a08174b1100661e1adace
ReneeR79 <renee.rosenheim@gmail.com>
2026-01-20 01:54:17 +0000

    done

--- src/main/java/com/BrickBreak/ Paddle.java
+++ src/main/java/com/BrickBreak/ Paddle.java
@@ -1,21 +1,91 @@
+// Renee Rosenheim
+// 1/9/26
+// Creates a paddle for the game BrickBreak
 package com.BrickBreak;
 
 import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
-
-
-	//your code here!
-
-
-	
+	//your code her	
 	//don't forget you need instance variables:
-	
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int speed;
+	private int location;
 	
 	//constructor(s):
-	
+	public Paddle(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.speed = 1;
+		this.location = x + width /2;
+	}
+
 	//methods:
+	public void draw(Graphics g){
+		g.setColor(Color.WHITE);
+		g.fillRect(x, y, width, height);
+	}
+
+	public void move(){
+		x += speed;
+
+		if (x < 0 || x > 700 - width){
+			System.out.println("hit left wall");
+			speed = -speed;
+		}
+	}
+
+	public void setLocation(int loc){
+		this.location = loc;
+	}
+
+	public int getLocation(){
+		return location;
+	}
+
+	public int getVelocity(){
+		return speed;
+	}
+
+	public void addVelocity(int input){
+		this.speed += input;
+	}
+
+	public int setVelocity(int input){
+		this.speed += input;
+	}
 	
-	
-}
+	public void setX(int x){
+		this.x = x;
+	}
+
+	public int getX(){
+		return x;
+	}
+
+	public void sety(int y){
+		this.y = y;
+	}
+
+	public int getY(){
+		return y;
+	}
+
+	public int getHeight(){
+		return height;
+	}
+
+	public int getWidth(){
+		return width;
+	}
+
+	public int addVelocity(){
+		return speed;
+	}
+}	
--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -1,15 +1,93 @@
+// Renee Rosenheim
+// 1/9/26
+// Creates a ball for the game BrickBreak
 package com.BrickBreak;
 import java.awt.*;
 
 public class Ball {
 	//your code here!
-	
 	//don't forget you need instance variables:
-	
-	
-	
+	private int x;
+	private int y;
+	private int radius;
+	private int xVelocity;
+	private int yVelocity;
 	//constructor(s):
-	
+	public Ball9int x, int y, int radius){
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.xVelocity = -1;
+		this.yVelocity = -2;
+	}
 	//methods:
+	public void draw(Graphics g) {
+		g.setColor(Color.PINK);
+		g.fillOval(x, y, radius, radius);
+	}
 
+	public void move(){
+		x+= xVelocity;
+		y+= yVelocity;
+	}
+
+	public void setX(int x){
+		this.x = x;
+	}
+
+	public void setY(int y){
+		this.y = y;
+	}
+
+	public int getX(){
+		return x;
+	}
+
+	public int getY(){
+		return y;
+	}
+
+	public void reverseX(){
+		this.xVelocity = -this.xVelocity;
+	}
+
+	public int getXpos(){
+		return x;
+	}
+
+	public int getYpos(){
+		return y;
+	}
+
+	public void reverseY(){
+		this.yVelocity = -this.yVelocity;
+	}
+
+	public void setXVelocity(int xVelocity){
+		this.xVelocity = xVelocity;
+	}
+
+	public void setYVelocity(int yVelocity){
+		this.yVelocity = yVelocity;
+	}
+
+	public int getXvelocity(){
+		return xVelocity;
+	}
+
+	public void setYvelocity(int yVelocity){
+		this.yVelocity = yVelocity;
+	}
+
+	public int getYvelocity(){
+		return xVelocity;
+	}
+
+	public void setSize(int radius){
+		this.radius = radius;
+	}
+
+	public in getSize(){
+		return radius;
+	}
 }
